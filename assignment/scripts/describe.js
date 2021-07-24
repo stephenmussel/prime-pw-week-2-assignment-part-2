84be4200ead4ec8aec32a7008d33a4e706a756dc
Phaydara Vongsavanthong <stephen.mussel@gmail.com>
2021-07-24 17:52:27 -0500

    comments added to question 4

--- assignment/scripts/describe.js
+++ assignment/scripts/describe.js
@@ -138,6 +138,9 @@ if (number >= 2) {
 // -- it is, so we set colorOne and colorTwo to 'purple'
 
 /*
+
+// FIX - colorOne should be set to blue as a string and colorTwo should be set to red as a string
+// Should be number++.
 let colorOne = 'red';
 let colorTwo = 'blue';
 let mix = true;
